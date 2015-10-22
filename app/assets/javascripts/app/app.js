f2b68d9e647e144849f061ee1a2517806243a540
Dimas Julisa Taniawan <dimas@41studio.com>
2015-10-22 09:48:44 +0700

    fix for production

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -8,74 +8,76 @@ var app = angular.module("BottleApp", [
 ]);
 
 // routes
-app.config(function($routeProvider, $locationProvider, $authProvider){
-  $authProvider.configure({
-    apiUrl:                  '/api',
-    tokenValidationPath:     '/auth/validate_token',
-    signOutUrl:              '/auth/sign_out',
-    emailRegistrationPath:   '/auth',
-    accountUpdatePath:       '/auth',
-    accountDeletePath:       '/auth',
-    confirmationSuccessUrl:  window.location.href,
-    passwordResetPath:       '/auth/password',
-    passwordUpdatePath:      '/auth/password',
-    passwordResetSuccessUrl: window.location.href,
-    emailSignInPath:         '/auth/sign_in',
-    storage:                 'cookies',
-    forceValidateToken:      false,
-    validateOnPageLoad:      true,
-    proxyIf:                 function() { return false; },
-    proxyUrl:                '/proxy',
-    omniauthWindowType:      'sameWindow',
-    authProviderPaths: {
-      facebook: '/auth/facebook'
-    },
-    tokenFormat: {
-      "access-token": "{{ token }}",
-      "token-type":   "Bearer",
-      "client":       "{{ clientId }}",
-      "expiry":       "{{ expiry }}",
-      "uid":          "{{ uid }}"
-    },
-    parseExpiry: function(headers) {
-      // convert from UTC ruby (seconds) to UTC js (milliseconds)
-      return (parseInt(headers['expiry']) * 1000) || null;
-    },
-    handleLoginResponse: function(response) {
-      return response.data;
-    },
-    handleAccountUpdateResponse: function(response) {
-      return response.data;
-    },
-    handleTokenValidationResponse: function(response) {
-      return response.data;
-    }
-  });
+app.config(['$routeProvider', '$locationProvider', '$authProvider',
+  function($routeProvider, $locationProvider, $authProvider){
+    $authProvider.configure({
+      apiUrl:                  '/api',
+      tokenValidationPath:     '/auth/validate_token',
+      signOutUrl:              '/auth/sign_out',
+      emailRegistrationPath:   '/auth',
+      accountUpdatePath:       '/auth',
+      accountDeletePath:       '/auth',
+      confirmationSuccessUrl:  window.location.href,
+      passwordResetPath:       '/auth/password',
+      passwordUpdatePath:      '/auth/password',
+      passwordResetSuccessUrl: window.location.href,
+      emailSignInPath:         '/auth/sign_in',
+      storage:                 'cookies',
+      forceValidateToken:      false,
+      validateOnPageLoad:      true,
+      proxyIf:                 function() { return false; },
+      proxyUrl:                '/proxy',
+      omniauthWindowType:      'sameWindow',
+      authProviderPaths: {
+        facebook: '/auth/facebook'
+      },
+      tokenFormat: {
+        "access-token": "{{ token }}",
+        "token-type":   "Bearer",
+        "client":       "{{ clientId }}",
+        "expiry":       "{{ expiry }}",
+        "uid":          "{{ uid }}"
+      },
+      parseExpiry: function(headers) {
+        // convert from UTC ruby (seconds) to UTC js (milliseconds)
+        return (parseInt(headers['expiry']) * 1000) || null;
+      },
+      handleLoginResponse: function(response) {
+        return response.data;
+      },
+      handleAccountUpdateResponse: function(response) {
+        return response.data;
+      },
+      handleTokenValidationResponse: function(response) {
+        return response.data;
+      }
+    });
 
-  $routeProvider
-    .when('/', {
-      templateUrl: 'main/index.html',
-      controller: 'MainController'
-    })
-    .when('/products/:id', {
-      templateUrl: 'products/show.html',
-      controller: 'ProductController'
-    })
-    .when('/login', {
-      templateUrl: 'users/login.html',
-      controller: 'UsersController'
-    })
-    .when('/register', {
-      templateUrl: 'users/register.html',
-      controller: 'UsersController'
-    })
-    .when('/profile', {
-      templateUrl: 'users/profile.html',
-      controller: 'UsersController'
-    })
+    $routeProvider
+      .when('/', {
+        templateUrl: 'main/index.html',
+        controller: 'MainController'
+      })
+      .when('/products/:id', {
+        templateUrl: 'products/show.html',
+        controller: 'ProductController'
+      })
+      .when('/login', {
+        templateUrl: 'users/login.html',
+        controller: 'UsersController'
+      })
+      .when('/register', {
+        templateUrl: 'users/register.html',
+        controller: 'UsersController'
+      })
+      .when('/profile', {
+        templateUrl: 'users/profile.html',
+        controller: 'UsersController'
+      })
 
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
-});
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  }
+]);
